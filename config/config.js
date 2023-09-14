6911440d03c5fdf23d299f86bfd505569cc5a07c
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-09-13 20:11:05 -0700

    added DBHOST

--- config/config.js
+++ config/config.js
@@ -6,21 +6,21 @@ module.exports =
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
-    "dialect": "postgres v4.0.0"
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   }
 }
