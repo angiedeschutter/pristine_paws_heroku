9ad13de4224f2cc63a8de12090c93b18836f5a2f
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-09-13 19:06:19 -0700

    removed version to dialect

--- config/config.js
+++ config/config.js
@@ -14,13 +14,13 @@ module.exports =
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
-    "dialect": "postgres v4.0.0"
+    "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
-    "dialect": "postgres v4.0.0"
+    "dialect": "postgres"
   }
 }
