ac781f76982232e6d254a2d7bd0d1d441f82538a
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-09-16 14:22:54 -0700

    fixed logo

--- front_end/src/Components/NavBar.jsx
+++ front_end/src/Components/NavBar.jsx
@@ -30,6 +30,31 @@ export default function NavigationBar() {
         )
     }
 
+    
+let open='/puppy1.jpg'
+let closed='/puppy2.jpg'
+let puppyLogo = document.getElementById('logo')
+
+function appendlogo(image){
+    document.getElementById('logo').src=image
+}
+
+//This is a promise function that will delay the appending of each image to create a sequence of movement
+function wait(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })
+}
+
+async function puppy(){
+    appendlogo(open)
+    await wait(2000)
+    appendlogo(closed)
+}
+//This function repeats the animation function every second
+setInterval(puppy,4000) 
+
+
     return (
         <>
             <Navbar>
